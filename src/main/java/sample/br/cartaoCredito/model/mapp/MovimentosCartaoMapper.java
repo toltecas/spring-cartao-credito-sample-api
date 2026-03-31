60dbb2b91067475223cb41a37c98279e22166159
Carlos Rocha <60583506+toltecas@users.noreply.github.com>
2026-03-31 18:17:26 -0300

    1) Add metodo toDto(List<>).

--- src/main/java/sample/br/cartaoCredito/model/mapp/MovimentosCartaoMapper.java
+++ src/main/java/sample/br/cartaoCredito/model/mapp/MovimentosCartaoMapper.java
@@ -1,11 +1,21 @@
 package sample.br.cartaoCredito.model.mapp;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import sample.br.cartaoCredito.model.MovimentosCartao;
 import sample.br.cartaoCredito.model.dto.MovimentosCartaoDTO;
 
+import java.util.List;
+
 @Mapper(componentModel = "spring")
 public interface MovimentosCartaoMapper {
-    MovimentosCartaoDTO toDto(MovimentosCartao movimentos);
+
+    @Mapping(source = "membro.id", target = "codigoMembroFamilia")
+    MovimentosCartaoDTO toDto(MovimentosCartao movimento);
+
+    @Mapping(source = "membro.id", target = "codigoMembroFamilia")
+    List<MovimentosCartaoDTO> toDto(List<MovimentosCartao> movimentos);
+
+    @Mapping(source = "codigoMembroFamilia", target = "membro.id")
     MovimentosCartao toEntity(MovimentosCartaoDTO movimentosDTO);
 }
